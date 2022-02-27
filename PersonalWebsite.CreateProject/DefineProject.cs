34067f414808798aa2d490ade48914cca9be2d23
EU\dlopes <dlopes@neoception.com>
2022-02-27 19:38:44 +0000

    feat: create class to define a new project in the database

---
+++ PersonalWebsite.CreateProject/DefineProject.cs
@@ -0,0 +1,33 @@
+﻿using PersonalWebsite.DatabaseModel;
+
+namespace PersonalWebsite.CreateProject;
+
+public static class ProjectCreation
+{
+    private static int GetProjectIdFromDatabase(DatabaseContext context)
+    {
+        if (!context.ProjectDetails.Any())
+            return 1;
+
+        return context.ProjectDetails
+            .Select(pd => pd.ProjectId)
+            .Last() + 1;
+    }
+
+    public static List<ProjectDetails> DefineProject(DatabaseContext context,
+        int projectId, string projectName, string url)
+    {
+        List<ProjectDetails> project = new()
+        {
+            new ProjectDetails
+            {
+                ProjectId = GetProjectIdFromDatabase(context),
+                Name = projectName,
+                Url = url,
+                Date = DateTime.Now
+            }
+        };
+
+        return project;
+    }
+}
