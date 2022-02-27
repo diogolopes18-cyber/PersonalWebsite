54854d01f289ad5633f57d57a2ebaffbc0752a19
EU\dlopes <dlopes@neoception.com>
2022-02-27 18:17:31 +0000

    feat: create new personal website project

---
+++ PersonalWebsite.DatabaseModel/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalWebsite.DatabaseModel;
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) : base(options) {}
+    
+    public DbSet<ProjectDetails> ProjectDetails { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProjectDetails>()
+            .HasKey(pd => pd.ProjectId);
+    }
+}
+
+public static class DbInitializer
+{
+    public static void Initialize(DatabaseContext context)
+    {
+        context.Database.Migrate();
+    }
+}
---
+++ PersonalWebsite.DatabaseModel/ProjectDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalWebsite.DatabaseModel
+{
+    public class ProjectDetails
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
+
