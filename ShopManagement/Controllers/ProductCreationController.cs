5bde78162e91d33bac6e91c78243581a2f318eb7
EU\dlopes <dlopes@neoception.com>
2022-03-07 18:01:33 +0000

    feat: create controller to define a new product

---
+++ ShopManagement/Controllers/ProductCreationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ShopManagement.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class ProductCreationController : Controller
+{
+    private readonly DatabaseContext _context;
+
+    public ProductCreationController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult AddProduct() => View();
+
+    [HttpPost]
+    public IActionResult CreateProduct()
+    {
+        string name = Request.Form["productName"];
+        string tag = Request.Form["tag"];
+        ProductCreationControllerHandler.CreateProduct(name, tag, _context);
+        
+        
+        return Redirect("http://localhost:5000");
+    }
+}
---
+++ ShopManagement/Pages/AddProduct.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopManagement.Pages;
+public class AddProduct : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
