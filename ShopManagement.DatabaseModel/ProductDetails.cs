340c7cd44709f4536f367405b1e8bba336d53e57
EU\dlopes <dlopes@neoception.com>
2022-03-07 17:56:20 +0000

    fix: set ID as non automatic generated ID

--- ShopManagement.DatabaseModel/ProductDetails.cs
+++ ShopManagement.DatabaseModel/ProductDetails.cs
@@ -1,7 +1,10 @@
-﻿namespace PersonalWebsite.DatabaseModel;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopManagement.DatabaseModel;
 
 public class ProductDetails
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int ProductId { get; set; }
     public string Name { get; set; }
     public string Tag { get; set; }
