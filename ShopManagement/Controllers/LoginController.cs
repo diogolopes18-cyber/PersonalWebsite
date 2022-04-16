d19c83da62a2852ba12fe4c51611b8081ec00d49
EU\dlopes <dlopes@neoception.com>
2022-04-16 21:55:44 +0100

    feat: define Login controller

---
+++ ShopManagement/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopManagement.DatabaseModel;
+
+namespace ShopManagement.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class LoginController : Controller
+{
+    private readonly DatabaseContext _dbContext;
+
+    public LoginController(DatabaseContext context)
+    {
+        _dbContext = context;
+    }
+    
+    [HttpGet]
+    public IActionResult Login() => View();
+
+    [HttpPost]
+    public IActionResult UserLogin()
+    {
+        LoginControllerHandler.SaveUserDetails(Request.Form["username"], 
+            Request.Form["password"],Request.Form["confirmedPassword"],
+            Request.Form["email"], _dbContext);
+        
+        return Ok();
+    }
+}
