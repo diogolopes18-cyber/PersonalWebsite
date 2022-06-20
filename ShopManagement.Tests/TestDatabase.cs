56e2ec1041f2c6c4b522455d1aedc43a4b56ca7f
EU\dlopes <dlopes@neoception.com>
2022-06-20 19:03:58 +0100

    feat: define a mock database context for unit testing

---
+++ ShopManagement.Tests/TestDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopManagement.DatabaseModel;
+
+namespace ShopManagement.Tests;
+
+public class TestDatabaseFixture
+{
+    private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=ShopManagement_Test;Trusted_Connection=True";
+
+    private static readonly object _lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using (var context = CreateContext())
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    
+    public static DatabaseContext CreateContext()
+        => new DatabaseContext(
+            new DbContextOptionsBuilder<DatabaseContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+}
