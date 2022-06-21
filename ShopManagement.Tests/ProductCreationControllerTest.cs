f9cc845808d9b08a77daea4b8bcf56e2abc8d435
EU\dlopes <dlopes@neoception.com>
2022-06-21 10:11:36 +0100

    test: use handler class to retrieve all products

--- ShopManagement.Tests/ProductCreationControllerTest.cs
+++ ShopManagement.Tests/ProductCreationControllerTest.cs
@@ -9,15 +9,14 @@ public class ProductCreationControllerTest : IClassFixture<TestDatabaseFixture>
     public ProductCreationControllerTest(TestDatabaseFixture fixture)
         => Fixture = fixture;
 
-    public TestDatabaseFixture Fixture { get; }
+    private TestDatabaseFixture Fixture { get; }
     
     [Fact]
     public void ProductHasName()
     {
         using DatabaseContext context = TestDatabaseFixture.CreateContext();
-        var controller = new ProductCreationController(context);
 
-        var productName = controller.GetProducts();
+        var productName = ProductCreationControllerHandler.GetProducts(context);
         Assert.NotEmpty(productName);
     }
 }
