4e1df48052e1d3c3a1069abc9a3ea79c4378ab73
EU\dlopes <dlopes@neoception.com>
2022-06-21 17:17:21 +0100

    test: create seeder class to feed mock database

---
+++ ShopManagement.Tests/DatabaseSeeder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JsonNet.ContractResolvers;
+using Newtonsoft.Json;
+using ShopManagement.DatabaseModel;
+
+namespace ShopManagement.Tests;
+
+public static class Seeder {
+    public static void Seed(string jsonData,
+        DatabaseContext context) {
+        JsonSerializerSettings? settings = new JsonSerializerSettings {
+            ContractResolver = new PrivateSetterContractResolver()
+        };
+        
+        var events =
+            JsonConvert.DeserializeObject<List<ProductDetails>>(
+                jsonData, settings);
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        if (!context.ProjectDetails.Any())
+        {
+            if (events != null) context.AddRange(events);
+            context.SaveChanges();
+        }
+    }
+}
