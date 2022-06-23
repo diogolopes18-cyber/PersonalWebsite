bb276820821ca9501d431bc68b1252d72dd8d838
EU\dlopes <dlopes@neoception.com>
2022-06-23 16:42:00 +0100

    feat: create new data model for storing drink consumption

---
+++ ShopManagement.DatabaseModel/ConsumptionRegister.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopManagement.DatabaseModel;
+
+public class ConsumptionRegister
+{
+    public string Username { get; set; }
+    public string DrinkType { get; set; }
+    public DateTime date { get; set; }
+}
