31f831bf08580674e1e22c72d01a347be87f2393
EU\dlopes <dlopes@neoception.com>
2022-06-23 16:42:19 +0100

    feat: define UI for drink registration

---
+++ ShopManagement/Pages/ConsumptionRegister.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopManagement.Pages;
+
+public class ConsumptionRegistration : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
