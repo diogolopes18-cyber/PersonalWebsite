118d278f287b372d761ff14e116f386f06e1d799
EU\dlopes <dlopes@neoception.com>
2022-08-02 19:03:46 +0100

    refactor: remove deprecated controllers

--- ShopManagement.DatabaseModel/ConsumptionRegister.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShopManagement.DatabaseModel;
-
-public class ConsumptionRegister
-{
-    public string Username { get; set; }
-    public string DrinkType { get; set; }
-    public DateTime date { get; set; }
-}
